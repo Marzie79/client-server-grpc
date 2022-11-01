3efc35d6e89ecd4671e4e3c37d84b992d4710d7f
marzie79 <marzie.7900@gmail.com>
2022-11-01 18:59:25 +0330

    fix env

--- client/utilities/stream/producers/client_service.py
+++ client/utilities/stream/producers/client_service.py
@@ -29,11 +29,9 @@ class Client:
                 routing_key=queue_.routing_key,
                 declare=[queue_],
                 retry=True,
-                # userid="hello", 
-                # password="bye",
             )
             print("here")
 
-import json
-from utilities.stream.producers.client_service import Client
-Client.send_log({"hello": "bye"})
+# import json
+# from utilities.stream.producers.client_service import Client
+# Client.send_log({"hello": "bye"})
